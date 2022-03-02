2dd3129f504b8c94495ae4d4aac9c1b74c1a2ced
Ezra Cerpac <ecerpac@rocketmail.com>
2022-03-02 12:17:12 +0100

    Scintilation probability density function completed!

--- combined_fit/rytov_index.py
+++ combined_fit/rytov_index.py
@@ -1,19 +1,43 @@
 import pickle
 
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy import integrate
 
 
 def main():
+    with open('../Data/DFs/data11/off1.pickle', 'rb') as f:
+        data = pickle.load(f)
+    I = np.array(data)
+    ii = np.linspace(0, 1, 1001)
+    probs = calc_probs(I, ii)
+
+    plt.plot(ii, probs)
+    plt.show()
+
+
+def calc_probs(I: np.ndarray, ii: np.ndarray) -> np.ndarray:
     with open('../Data/DFs/Cn.pickle', 'rb') as f:
         Cn = pickle.load(f)
 
-    zz = Cn['z-distance']
-    C_n2 = Cn['Cn^2']
+    zz = np.array(Cn['z-distance'])
+    C_n2 = np.array(Cn['Cn^2'])
 
-    labda = 1e-6
+    labda = 1e-6  # Wavelength (unknown)
+
     sigma_R2 = rytov_index(k(labda), zz, C_n2)
     sigma_I2 = scintillation_index(sigma_R2)
+
+    norm_I = I / max(I)
+    I_0 = norm_I.mean()
+    return probability_dist(ii, I_0, sigma_I2)  # return array
+
+
+def probability_dist(I:float, I_0:float, sigma_I2:float) -> float:
+    return 1 / (I * sigma_I2 * np.sqrt(2 * np.pi)) * np.exp(
+        - (np.log(I / I_0) + 0 + sigma_I2 / 2) ** 2 / (2 * sigma_I2)
+    )
+
 
 
 
@@ -22,16 +46,16 @@ k = lambda l: 2 * np.pi / l  # l = lambda
 
 def rytov_index(k, zz: np.ndarray, C_n2: np.ndarray) -> float:
     coef = 2.25 * k ** (7 / 6)
-    yy = [C_n2[i] * (max(zz) - z) ** (5 / 6) for i, z in enumerate(zz)]
+    yy = [C_n2[i] * (zz[-1] - z) ** (5 / 6) for i, z in enumerate(zz)]
     integral = integrate.simpson(yy, zz)
     return coef * integral
 
 
 def scintillation_index(sigma_R2) -> float:
     return np.exp(
-        0.49 * sigma_R2 / (1 + 1.11 * sigma_R2 ** (6/5)) ** (7/6) //
-        + 0
-    )
+        0.49 * sigma_R2 / (1 + 1.11 * sigma_R2 ** (6 / 5)) ** (7 / 6) +
+        0.51 * sigma_R2 / (1 + 0.69 * sigma_R2 ** (6 / 5)) ** (5 / 6)
+    ) - 1
 
 
 if __name__ == '__main__':

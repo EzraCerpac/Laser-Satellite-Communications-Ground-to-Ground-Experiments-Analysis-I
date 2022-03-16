e5b066136d973a95059ffb886e3fa9ba94283c3d
Bartosz Jemioł <ekstrabartek@gmail.com>
2022-03-16 12:14:20 +0100

    try, catch in scintillation.py

--- formula/jitter.py
+++ formula/jitter.py
@@ -5,5 +5,4 @@ def calc_sigma(beta: float, w_0: float) -> float:
     sigma = np.sqrt(w_0 ** 2 / (4 * beta))
     return sigma
 
-
 def k(labda: float) -> float: return 2 * np.pi / labda  # labda = lambda

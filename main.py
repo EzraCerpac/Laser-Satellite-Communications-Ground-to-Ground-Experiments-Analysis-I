13eb7e87bf1358829af2ef3103c0c087b04d1215
DidierVDVoort <didier.vdvoort@gmail.com>
2022-03-18 12:28:39 +0100

    Sigma plot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import pickle
 import warnings
 
+from combined_fit.Plotting.plots import sigmaplot
 from conf.config import FileConfig
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
@@ -8,8 +10,18 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 def main(cfg=FileConfig()):
     # cfg.set_data(18, True, 8)
     # cfg.run().calc_sigma(plot=True)
-    print(cfg.run_batch([18]).run('sigma'))
+    # cfg.run_batch([18]).run('sigma')
 
+    results = open_results()
+    sigmaplot(results)
+
+def store_results(cfg=FileConfig()):
+    with open('Results/sigmas.pickle', 'wb') as f:
+        pickle.dump(cfg.run_batch([18]).run('sigma'), f)
+
+def open_results(file: str = 'Results/sigmas.pickle') -> dict:
+    with open(file, 'rb') as f:
+        return pickle.load(f)
 
 if __name__ == "__main__":
     main()

839e81e41553ae589372663f63bea2fe6f29db38
Ezra Cerpac <ecerpac@rocketmail.com>
2022-03-23 13:35:20 +0100

    Not working combined fit fit

--- Model/main.py
+++ Model/main.py
@@ -1,23 +1,43 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.integrate import quad
+from scipy.optimize import curve_fit
+from scipy.stats import rv_continuous
 
 from combined_fit.angular_jitter_fit_beta import beta_func
-from combined_fit.indices import scintillation_index, rytov_index, rytov_index_const
+from combined_fit.indices import scintillation_index, rytov_index
 from combined_fit.scintillation import probability_dist
 from formula.jitter import k
+from info_plots.norm_I_hist import norm_I_hist
 
-beta = 3
-labda = 1550e-9
-
-Cn = pd.read_pickle('../Data/DFs/Cn.pickle')
+Cn = pd.read_pickle('Data/DFs/Cn.pickle')
 zz = np.array(Cn['z-distance'])
 C_n2 = np.array(Cn['Cn^2'])
 
 
-def main():
-    ii = np.linspace(1e-10, 1, 1000)
-    II = np.linspace(1e-10, 1, 50)
+class CombinedFit(rv_continuous):
+    # def __init__(self, **kwargs):
+    #     super(CombinedFit, self).__init__(*kwargs)
+
+    def _pdf(self, X, beta):
+        return [quad(lambda I: beta_func(I, beta[0]) * probability_dist(
+            x, I, scintillation_index(rytov_index(
+                k(labda), zz, C_n2
+            ))), 0, 1)[0] for x in X] if hasattr(X, '__iter__') \
+            else quad(lambda I: beta_func(I, beta) * probability_dist(
+            X, I, scintillation_index(rytov_index(
+                k(labda), zz, C_n2
+            ))), 0, 1)[0]
+
+
+beta = 3
+labda = 1550e-9
+
+
+def main_colors():
+    ii = np.linspace(1e-10, 4, 1000)
+    II = np.linspace(0, 1, 1000)
     ff = np.zeros(len(ii))
     for i in II:
         f = beta_func(i, beta) * probability_dist(
@@ -33,14 +53,33 @@ def main():
     print(np.sum(ff) * (ii[1] - ii[0]))
 
 
-def test():
-    ii = np.linspace(1e-18, 1, 1000)
-    ff = probability_dist(
-        ii, 0.5, scintillation_index(rytov_index_const(
-            k(labda), max(zz), C_n2.mean()
-        )))
+def main():
+    ii = np.linspace(1e-10, 4, 1000)
+    ff = [quad(lambda I: beta_func(I, beta) * probability_dist(
+        i, I, scintillation_index(rytov_index(
+            k(labda), zz, C_n2
+        ))), 0, 1)[0] for i in ii]
+    plt.plot(ii, ff, color='black', linewidth=4)
+    plt.show()
+
     print(np.sum(ff) * (ii[1] - ii[0]))
 
 
+def combined_dist(i: float, beta: float, scale: float = 1):
+    return quad(lambda I: beta_func(I, beta) * probability_dist(
+        i, I, scintillation_index(rytov_index(
+            k(labda), zz, C_n2
+        ))), 0, 1)[0] / scale
+
+
+def main2(irradiance):
+    yy = norm_I_hist(irradiance, bins=100)
+    xx = np.linspace(0, 1, len(yy))
+    p_opt, p_error = curve_fit(CombinedFit().pdf, xx, yy, p0=[2])
+    beta, scale = p_opt
+    plt.plot(xx, [CombinedFit().pdf(x, beta) for x in xx])
+    plt.show()
+
+
 if __name__ == '__main__':
-    test()
+    main2()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import warnings
 
+import numpy as np
+
+from Model.main import main2
 from conf.config import Config
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
@@ -8,8 +11,8 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 def main(cfg=Config()):
     # cfg.set_data(18, True, 8)
     # cfg.run().calc_sigma(plot=True)
-    print(cfg.run_batch([18]).run('sigma', plot=True))
-
+    # print(cfg.run_batch([18]).run('sigma', plot=True))
+    main2(np.array(cfg.set_data(18, True, 16).df))
 
 if __name__ == "__main__":
     main()

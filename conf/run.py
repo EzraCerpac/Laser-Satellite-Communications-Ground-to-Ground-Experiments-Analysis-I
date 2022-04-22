cc21df02a112a91eac9644de0aa2a5f889a3a1a4
Ezra Cerpac <ecerpac@rocketmail.com>
2022-04-22 15:09:46 +0200

    multiprocessing

--- conf/run.py
+++ conf/run.py
@@ -1,4 +1,5 @@
-from typing import Dict
+import multiprocessing as mp
+from typing import Dict, List, Tuple, Any
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,6 +8,8 @@ from Model.main import estimate_sigma
 from Model.main_without_scale import estimate_sigma as estimate_sigma_with_alpha
 from conf.data import Data
 from info_plots.norm_I_hist import norm_I_hist
+
+Result = Dict[int, Dict[bool, Dict[int, Dict[str, float]]]]
 
 
 class Run:
@@ -84,9 +87,18 @@ class BatchRun:
     def __init__(self, data_sets):
         self.data = [[[Data(data_set, mode, number) for number in Data.mode_dict[mode]]
                       for mode in Data.mode_dict.keys()] for data_set in data_sets]
-        self.results: Dict[int, Dict[bool, Dict[int, Dict[str, float]]]] = {}
+        self.results: Result = {}
 
-    def run(self, *functions: str, **kwargs):
+    def run(self, *functions: str, **kwargs) -> Result:
+        """
+        Run all functions on all data sets
+        :param functions: functions to run
+        :param kwargs:
+            res: int, resolution of the histogram
+            plot: bool, if True, plot the histogram
+            save: bool, if True, save the plots
+        :return: Result object (dict)
+        """
         print('Running Programs: ' + ', '.join([function for function in functions]))
         for i, data_set in enumerate(self.data):
             print(f'Running experiment {i + 1} of {len(self.data)}')
@@ -94,22 +106,50 @@ class BatchRun:
             for j, data_mode in enumerate(data_set):
                 number_results = {}
                 for k, data in enumerate(data_mode):
-                    print(f'\tRunning dataset {j * len(data_mode) + (k + 1)} of {len(data_mode) * len(data_set)}')
-                    run = Run(data)
-                    function_dict = {
-                        'sigma': run.calc_sigma,
-                        'sigma gamma': run.calc_sigma_gamma,
-                        'sigma_with_alpha': run.calc_sigma_with_alpha,
-                        'sigma_gamma_with_alpha': run.calc_sigma_gamma_with_alpha,
-                    }
-                    try:
-                        [function_dict[function](**kwargs) for function in functions]
-                        if 'plot' in kwargs and kwargs['plot']:
-                            run.plot(True if 'save' in kwargs and kwargs['save'] else False)
-                        number_results[data.number] = run.results
-                    except RuntimeError:
-                        print(f'RuntimeError in dataset {j * len(data_mode) + (k + 1)}')
-                        plt.clf()
+                    number_results[data.number] = \
+                    self._run(data, data_mode, data_set, functions, j, k, kwargs, number_results)[1]
                 mode_results[data_mode[0].mode] = number_results
             self.results[data_set[0][0].data_set] = mode_results
         return self.results
+
+    def run_parallel(self, *functions: str, **kwargs) -> List[Tuple[Any, Dict[Any, Any]]]:
+        """
+        Run all functions on all data sets in parallel
+        :param functions: functions to run
+        :param kwargs:
+            res: int, resolution of the histogram
+            plot: bool, if True, plot the histogram
+            save: bool, if True, save the plots
+        :return: Result object (dict)
+        """
+        pool = mp.Pool(mp.cpu_count())
+        results = []
+        print('Running Programs: ' + ', '.join([function for function in functions]))
+        for i, data_set in enumerate(self.data):
+            print(f'Running experiment {i + 1} of {len(self.data)}')
+            for j, data_mode in enumerate(data_set):
+                for k, data in enumerate(data_mode):
+                    results.append(
+                        pool.apply_async(self._run, args=(data, data_mode, data_set, functions, j, k, kwargs)))
+        results = [result.get() for result in results]
+        pool.close()
+        return results
+
+    @staticmethod
+    def _run(data, data_mode, data_set, functions, j, k, kwargs) -> (Data, Result):
+        print(f'\tRunning dataset {j * len(data_mode) + (k + 1)} of {len(data_mode) * len(data_set)}')
+        run = Run(data)
+        function_dict = {
+            'sigma': run.calc_sigma,
+            'sigma gamma': run.calc_sigma_gamma,
+            'sigma_with_alpha': run.calc_sigma_with_alpha,
+            'sigma_gamma_with_alpha': run.calc_sigma_gamma_with_alpha,
+        }
+        try:
+            [function_dict[function](**kwargs) for function in functions]
+            if 'plot' in kwargs and kwargs['plot']:
+                run.plot(True if 'save' in kwargs and kwargs['save'] else False)
+            return data, run.results
+        except RuntimeError:
+            print(f'RuntimeError in dataset {j * len(data_mode) + (k + 1)}')
+            plt.clf()
